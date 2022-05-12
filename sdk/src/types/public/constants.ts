1d867a014b15e03eb4ca8b19caa216503b256b17
Otto Cheung <otto@orca.so>
2022-05-12 08:54:57 -0700

    Add Orca Whirlpools constants

--- sdk/src/types/public/constants.ts
+++ sdk/src/types/public/constants.ts
@@ -1,27 +1,51 @@
 import { PublicKey } from "@solana/web3.js";
 
 /**
+ * Program ID hosting Orca's Whirlpool program.
+ * @category Constants
+ */
+export const ORCA_WHIRLPOOL_PROGRAM_ID = new PublicKey(
+  "whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc"
+);
+
+/**
+ * Orca's WhirlpoolsConfig PublicKey.
+ * @category Constants
+ */
+export const ORCA_WHIRLPOOLS_CONFIG = new PublicKey("2LecshUwdy9xi7meFgHtFJQNSKk4KdTrcpvaB56dP2NQ");
+
+/**
  * The number of rewards supported by this whirlpool.
  * @category Constants
  */
 export const NUM_REWARDS = 3;
 
 /**
- * @category Constants
- */
-export const MAX_TICK_ARRAY_CROSSINGS = 2;
-
-/**
+ * The maximum tick index supported by the Whirlpool program.
  * @category Constants
  */
 export const MAX_TICK_INDEX = 443636;
 
 /**
+ * The minimum tick index supported by the Whirlpool program.
  * @category Constants
  */
 export const MIN_TICK_INDEX = -443636;
 
 /**
+ * The maximum sqrt-price supported by the Whirlpool program.
+ * @category Constants
+ */
+export const MAX_SQRT_PRICE = "79226673515401279992447579055";
+
+/**
+ * The minimum sqrt-price supported by the Whirlpool program.
+ * @category Constants
+ */
+export const MIN_SQRT_PRICE = "4295048016";
+
+/**
+ * The number of initialized ticks that a tick-array account can hold.
  * @category Constants
  */
 export const TICK_ARRAY_SIZE = 88;
@@ -29,16 +53,12 @@ export const TICK_ARRAY_SIZE = 88;
 /**
  * @category Constants
  */
-export const MAX_SQRT_PRICE = "79226673515401279992447579055";
-
-/**
- * @category Constants
- */
-export const MIN_SQRT_PRICE = "4295048016";
-
-/**
- * @category Constants
- */
 export const METADATA_PROGRAM_ADDRESS = new PublicKey(
   "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
 );
+
+/**
+ * The maximum number of tick-array crossing that can occur in a swap.
+ * @category Constants
+ */
+export const MAX_TICK_ARRAY_CROSSINGS = 2;

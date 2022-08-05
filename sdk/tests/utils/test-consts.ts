7f68972107ed04221cab3214c1e57f92d3e83e10
skrrb <skrrb@pm.me>
2022-08-06 00:11:33 +0200

    sdk tests: fix MAX_U64 const definition

--- sdk/tests/utils/test-consts.ts
+++ sdk/tests/utils/test-consts.ts
@@ -7,4 +7,4 @@ export const ZERO_BN = new anchor.BN(0);
 
 export const ONE_SOL = 1000000000;
 
-export const MAX_U64 = new u64(new anchor.BN(2).pow(new anchor.BN(64).sub(new anchor.BN(1))).toString());
+export const MAX_U64 = new u64(new anchor.BN(2).pow(new anchor.BN(64)).sub(new anchor.BN(1)).toString());

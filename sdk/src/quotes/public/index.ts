e33701f076cc31f638c69b2a97873e0a3c5fb9d7
Fuzzy Yeti <fuzzyyeti@github.com>
2022-09-21 22:59:59 -0600

    Add swap with dev fee to typedocs

--- sdk/src/quotes/public/index.ts
+++ sdk/src/quotes/public/index.ts
@@ -3,3 +3,4 @@ export * from "./decrease-liquidity-quote";
 export * from "./collect-fees-quote";
 export * from "./collect-rewards-quote";
 export * from "./swap-quote";
+export * from "./dev-fee-swap-quote";

9dbec10a35079808f38f1f800af31ced8b07f899
Otto Cheung <otto@orca.so>
2023-01-13 18:14:39 +0800

    remove comments

--- sdk/src/instructions/composites/collect-all-txn.ts
+++ sdk/src/instructions/composites/collect-all-txn.ts
@@ -11,14 +11,14 @@ import { convertListToMap } from "../../utils/txn-utils";
 import {
   addNativeMintHandlingIx,
   getTokenMintsFromWhirlpools,
-  resolveAtaForMints,
+  resolveAtaForMints
 } from "../../utils/whirlpool-ata-utils";
 import { updateFeesAndRewardsIx } from "../update-fees-and-rewards-ix";
 
 /**
  * Parameters to collect all fees and rewards from a list of positions.
  *
- * @category Async Instruction Types
+ * @category Instruction Types
  * @param positionAddrs - An array of Whirlpool position addresses.
  * @param receiver - The destination wallet that collected fees & reward will be sent to. Defaults to ctx.wallet key.
  * @param positionOwner - The wallet key that contains the position token. Defaults to ctx.wallet key.
@@ -32,7 +32,7 @@ export type CollectAllPositionAddressParams = {
 /**
  * Parameters to collect all fees and rewards from a list of positions.
  *
- * @category Async Instruction Types
+ * @category Instruction Types
  * @param positions - An array of Whirlpool positions.
  * @param receiver - The destination wallet that collected fees & reward will be sent to. Defaults to ctx.wallet key.
  * @param positionOwner - The wallet key that contains the position token. Defaults to ctx.wallet key.
@@ -46,7 +46,7 @@ export type CollectAllPositionParams = {
 /**
  * Common parameters between {@link CollectAllPositionParams} & {@link CollectAllPositionAddressParams}
  *
- * @category Async Instruction Types
+ * @category Instruction Types
  * @param receiver - The destination wallet that collected fees & reward will be sent to. Defaults to ctx.wallet key.
  * @param positionOwner - The wallet key that contains the position token. Defaults to ctx.wallet key.
  * @param positionAuthority - The authority key that can authorize operation on the position. Defaults to ctx.wallet key.

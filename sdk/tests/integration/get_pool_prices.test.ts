b47023f840c5d8d84498b1e29cd7d28ba0277cd3
Yutaro Mori <yutaro@orca.so>
2023-02-21 16:55:59 +0800

    Add tests

--- sdk/tests/integration/get_pool_prices.test.ts
+++ sdk/tests/integration/get_pool_prices.test.ts
@@ -19,7 +19,7 @@ import { u64 } from "@solana/spl-token";
 import Decimal from "decimal.js";
 import { MathUtil } from "@orca-so/common-sdk";
 
-describe.only("get_pool_prices", () => {
+describe("get_pool_prices", () => {
   const provider = anchor.AnchorProvider.env();
   const program = anchor.workspace.Whirlpool;
   const context = WhirlpoolContext.fromWorkspace(provider, program);
@@ -217,13 +217,76 @@ describe.only("get_pool_prices", () => {
     assert.equal(Object.keys(priceMap).length, 2);
   });
 
-  it(
-    "successfully calculates the price for one token which requires an indirect pool to calculate price"
-  );
+  it.only("successfully calculates the price for one token which requires an indirect pool to calculate price", async () => {
+    const aqConfig = getDefaultAquarium();
+    // Add a third token and account and a second pool
+    aqConfig.initMintParams.push({});
+    aqConfig.initTokenAccParams.push({ mintIndex: 2 });
+    aqConfig.initPoolParams.push({ mintIndices: [1, 2], tickSpacing: TickSpacing.Standard });
 
-  it(
-    "successfully calculates the price for two tokens which each require a different indirect pool to calculate price"
-  );
+    // Add tick arrays and positions
+    const aToB = false;
+    aqConfig.initTickArrayRangeParams.push({
+      poolIndex: 0,
+      startTickIndex: 22528,
+      arrayCount: 3,
+      aToB,
+    });
+    aqConfig.initTickArrayRangeParams.push({
+      poolIndex: 1,
+      startTickIndex: 22528,
+      arrayCount: 3,
+      aToB,
+    });
+    const fundParams: FundedPositionParams[] = [
+      {
+        liquidityAmount: new anchor.BN(10_000_000),
+        tickLowerIndex: 29440,
+        tickUpperIndex: 33536,
+      },
+    ];
+    aqConfig.initPositionParams.push({ poolIndex: 0, fundParams });
+    aqConfig.initPositionParams.push({ poolIndex: 1, fundParams });
+
+    const aquarium = (await buildTestAquariums(context, [aqConfig]))[0];
+    const { mintKeys, configParams } = aquarium;
+
+    const config = {
+      quoteTokens: [mintKeys[2].toBase58(), mintKeys[1].toBase58()],
+      tickSpacings: [TickSpacing.Standard],
+      programId: program.programId,
+      whirlpoolsConfig: configParams.configInitInfo.whirlpoolsConfigKeypair.publicKey,
+    };
+
+    const thresholdConfig = {
+      amountThreshold: new u64(100),
+      priceImpactThreshold: 1.05,
+    };
+
+    const mints = [mintKeys[0], mintKeys[1], mintKeys[2]];
+
+    // fetch PoolMap
+    const poolMap = await fetchPoolsForMints(context, mints, config);
+
+    // fetch TickArrayMap
+    const tickArrayMap = await fetchTickArraysForPools(context, poolMap);
+
+    const decimalsMap = await fetchDecimalsForMints(context, mints);
+
+    assert.equal(Object.keys(poolMap).length, 2);
+    assert.equal(Object.keys(tickArrayMap).length, 6);
+
+    const priceMap = calculatePoolPrices(
+      mints,
+      poolMap,
+      tickArrayMap,
+      decimalsMap,
+      config,
+      thresholdConfig
+    );
+
+    assert.equal(Object.keys(priceMap).length, 3);
+  });
 
   it("fails if missing mint accounts");
 

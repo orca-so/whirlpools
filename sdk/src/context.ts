4ffaee0a71b3360b947dff49977979bde6a59afb
yugure <109891005+yugure-orca@users.noreply.github.com>
2023-07-19 00:26:01 +0900

    add AccountResolverOpts

--- sdk/src/context.ts
+++ sdk/src/context.ts
@@ -24,6 +24,22 @@ export type WhirlpoolContextOpts = {
   userDefaultConfirmCommitment?: Commitment;
 };
 
+type CreateWrappedSolAccountMethod = "withSeed" | "withKeypair";
+
+/**
+ * Default settings used when resolving token accounts.
+ * @category Core
+ */
+export type AccountResolverOpts = {
+  createWrappedSolAccountMethod: CreateWrappedSolAccountMethod;
+  allowPDAOwnerAddress: boolean;
+};
+
+const DEFAULT_ACCOUNT_RESOLVER_OPTS: AccountResolverOpts = {
+  createWrappedSolAccountMethod: "withKeypair",
+  allowPDAOwnerAddress: false,
+};
+
 /**
  * Context for storing environment classes and objects for usage throughout the SDK
  * @category Core
@@ -37,6 +53,7 @@ export class WhirlpoolContext {
   readonly lookupTableFetcher: LookupTableFetcher | undefined;
   readonly opts: WhirlpoolContextOpts;
   readonly txBuilderOpts: TransactionBuilderOptions | undefined;
+  readonly accountResolverOpts: AccountResolverOpts;
 
   public static from(
     connection: Connection,
@@ -44,7 +61,8 @@ export class WhirlpoolContext {
     programId: PublicKey,
     fetcher: WhirlpoolAccountFetcherInterface = buildDefaultAccountFetcher(connection),
     lookupTableFetcher?: LookupTableFetcher,
-    opts: WhirlpoolContextOpts = {}
+    opts: WhirlpoolContextOpts = {},
+    accountResolverOpts: AccountResolverOpts = DEFAULT_ACCOUNT_RESOLVER_OPTS,
   ): WhirlpoolContext {
     const anchorProvider = new AnchorProvider(connection, wallet, {
       commitment: opts.userDefaultConfirmCommitment || "confirmed",
@@ -57,7 +75,8 @@ export class WhirlpoolContext {
       program,
       fetcher,
       lookupTableFetcher,
-      opts
+      opts,
+      accountResolverOpts
     );
   }
 
@@ -66,7 +85,8 @@ export class WhirlpoolContext {
     program: Program,
     fetcher: WhirlpoolAccountFetcherInterface = buildDefaultAccountFetcher(provider.connection),
     lookupTableFetcher?: LookupTableFetcher,
-    opts: WhirlpoolContextOpts = {}
+    opts: WhirlpoolContextOpts = {},
+    accountResolverOpts: AccountResolverOpts = DEFAULT_ACCOUNT_RESOLVER_OPTS,
   ) {
     return new WhirlpoolContext(
       provider,
@@ -74,7 +94,8 @@ export class WhirlpoolContext {
       program,
       fetcher,
       lookupTableFetcher,
-      opts
+      opts,
+      accountResolverOpts
     );
   }
 
@@ -83,7 +104,8 @@ export class WhirlpoolContext {
     programId: PublicKey,
     fetcher: WhirlpoolAccountFetcherInterface = buildDefaultAccountFetcher(provider.connection),
     lookupTableFetcher?: LookupTableFetcher,
-    opts: WhirlpoolContextOpts = {}
+    opts: WhirlpoolContextOpts = {},
+    accountResolverOpts: AccountResolverOpts = DEFAULT_ACCOUNT_RESOLVER_OPTS,
   ): WhirlpoolContext {
     const program = new Program(WhirlpoolIDL as Idl, programId, provider);
     return new WhirlpoolContext(
@@ -92,7 +114,8 @@ export class WhirlpoolContext {
       program,
       fetcher,
       lookupTableFetcher,
-      opts
+      opts,
+      accountResolverOpts
     );
   }
 
@@ -102,7 +125,8 @@ export class WhirlpoolContext {
     program: Program,
     fetcher: WhirlpoolAccountFetcherInterface,
     lookupTableFetcher?: LookupTableFetcher,
-    opts: WhirlpoolContextOpts = {}
+    opts: WhirlpoolContextOpts = {},
+    accountResolverOpts: AccountResolverOpts = DEFAULT_ACCOUNT_RESOLVER_OPTS,
   ) {
     this.connection = provider.connection;
     this.wallet = wallet;
@@ -113,6 +137,7 @@ export class WhirlpoolContext {
     this.lookupTableFetcher = lookupTableFetcher;
     this.opts = opts;
     this.txBuilderOpts = contextOptionsToBuilderOptions(this.opts);
+    this.accountResolverOpts = accountResolverOpts;
   }
 
   // TODO: Add another factory method to build from on-chain IDL

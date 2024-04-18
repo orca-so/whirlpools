10ac0cfc4c952355261aa0c640af2bcc8bae0b9f
Wilhelm Thieme <wjthieme@gmail.com>
2024-04-18 10:50:31 -0400

    Add security txt file to the whirlpool program

---
+++ programs/whirlpool/src/security.rs
@@ -0,0 +1,10 @@
+use solana_security_txt::security_txt;
+
+#[cfg(not(feature = "no-entrypoint"))]
+security_txt! {
+    name: "Orca Whirlpool program",
+    project_url: "https://orca.so",
+    contacts: "discord:https://discord.orca.so/,twitter:https://twitter.com/orca_so",
+    policy: "https://immunefi.com/bounty/orca/",
+    source_code: "https://github.com/orca-so/whirlpools"
+}

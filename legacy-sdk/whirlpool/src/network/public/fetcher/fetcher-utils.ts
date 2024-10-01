b797f931b0a277d78a56454b8eeca993f17abfdc
yugure <109891005+yugure-orca@users.noreply.github.com>
2024-10-01 15:59:34 +0900

    add getAllPositionAccountsByOwner

--- legacy-sdk/whirlpool/src/network/public/fetcher/fetcher-utils.ts
+++ legacy-sdk/whirlpool/src/network/public/fetcher/fetcher-utils.ts
@@ -1,14 +1,17 @@
 import type { Address } from "@orca-so/common-sdk";
 import { AddressUtil } from "@orca-so/common-sdk";
-import type { Connection } from "@solana/web3.js";
+import type { Connection, PublicKey } from "@solana/web3.js";
 import invariant from "tiny-invariant";
-import type { WhirlpoolData } from "../../../types/public";
+import type { PositionBundleData, PositionData, WhirlpoolData } from "../../../types/public";
 import {
   AccountName,
   WHIRLPOOL_CODER,
   getAccountSize,
 } from "../../../types/public";
 import { ParsableWhirlpool } from "../parsing";
+import { TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID, unpackAccount } from "@solana/spl-token";
+import { PDAUtil, PositionBundleUtil } from "../../../utils/public";
+import { IGNORE_CACHE, WhirlpoolContext } from "../../..";
 
 /**
  * Retrieve a list of whirlpool addresses and accounts filtered by the given params using
@@ -62,4 +65,163 @@ export async function getAllWhirlpoolAccountsForConfig({
       pool,
     ]),
   );
+}
+
+type PositionMap = {
+  positions: ReadonlyMap<string, PositionData>;
+  positionsWithTokenExtensions: ReadonlyMap<string, PositionData>;
+  positionBundles: {
+    positionBundleAddress: Address,
+    positionBundleData: PositionBundleData,
+    bundledPositions: ReadonlyMap<number, PositionData>
+  }[]
+};
+
+/**
+ * Retrieve a list of position addresses and accounts filtered by the given params.
+ * @category Network
+ *
+ * @param ctx The whirlpool context
+ * @param owner The owner of the positions
+ * @param includesPositions Whether to fetch positions
+ * @param includesPositionsWithTokenExtensions Whether to fetch positions with token extensions
+ * @param includesBundledPositions Whether to fetch bundled positions
+ * @returns The map of position addresses to position accounts
+ */
+export async function getAllPositionAccountsByOwner({
+  ctx,
+  owner,
+  includesPositions = true,
+  includesPositionsWithTokenExtensions = true,
+  includesBundledPositions = false,
+}: {
+  ctx: WhirlpoolContext;
+  owner: Address;
+  includesPositions: boolean;
+  includesPositionsWithTokenExtensions: boolean;
+  includesBundledPositions: boolean;
+}): Promise<PositionMap> {
+  const positions = !includesPositions
+    ? new Map()
+    : await findPositions(
+      ctx,
+      owner,
+      TOKEN_PROGRAM_ID,
+    );
+
+  const positionsWithTokenExtensions = !includesPositionsWithTokenExtensions
+    ? new Map()
+    : await findPositions(
+      ctx,
+      owner,
+      TOKEN_2022_PROGRAM_ID,
+    );
+
+  const positionBundles = !includesBundledPositions
+    ? []
+    : await findBundledPositions(
+      ctx,
+      owner,
+    );
+
+  return {
+    positions,
+    positionsWithTokenExtensions,
+    positionBundles,
+  };
+}
+
+async function findPositions(
+  ctx: WhirlpoolContext,
+  owner: Address,
+  tokenProgramId: Address,
+): Promise<ReadonlyMap<string, PositionData>> {
+  const tokenAccounts = await ctx.connection.getTokenAccountsByOwner(
+    AddressUtil.toPubKey(owner),
+    {
+      programId: AddressUtil.toPubKey(tokenProgramId)
+    }
+  );
+  
+  // Get candidate addresses for the position
+  const candidatePubkeys: PublicKey[] = [];
+  tokenAccounts.value.forEach((ta) => {
+    const parsed = unpackAccount(ta.pubkey, ta.account);
+    if (parsed.amount === 1n) {
+      const pda = PDAUtil.getPosition(ctx.program.programId, parsed.mint);
+      candidatePubkeys.push(pda.publicKey);
+    }
+  });  
+
+  // Fetch candidate accounts
+  const positionData = await ctx.fetcher.getPositions(candidatePubkeys, IGNORE_CACHE);
+
+  // Drop null
+  return new Map(
+    Array.from(positionData.entries())
+      .filter(([_, v]) => v !== null) as [string, PositionData][]
+  );
+}
+
+async function findBundledPositions(
+  ctx: WhirlpoolContext,
+  owner: Address,
+): Promise<{
+  positionBundleAddress: Address,
+  positionBundleData: PositionBundleData,
+  bundledPositions: ReadonlyMap<number, PositionData>
+}[]> {
+  const tokenAccounts = await ctx.connection.getTokenAccountsByOwner(
+    AddressUtil.toPubKey(owner),
+    {
+      programId: AddressUtil.toPubKey(TOKEN_PROGRAM_ID)
+    }
+  );
+
+  // Get candidate addresses for the position bundle
+  const candidatePubkeys: PublicKey[] = [];
+  tokenAccounts.value.forEach((ta) => {
+    const parsed = unpackAccount(ta.pubkey, ta.account);
+    if (parsed.amount === 1n) {
+      const pda = PDAUtil.getPositionBundle(ctx.program.programId, parsed.mint);
+      candidatePubkeys.push(pda.publicKey);
+    }
+  });
+
+  // Fetch candidate accounts
+  const positionBundleData = await ctx.fetcher.getPositionBundles(candidatePubkeys, IGNORE_CACHE);
+
+  // Drop null
+  const positionBundles = Array.from(positionBundleData.entries())
+    .filter(([_, v]) => v !== null) as [string, PositionBundleData][];
+
+  const bundledPositionPubkeys: PublicKey[] = [];
+  positionBundles.forEach(([_, positionBundle], i) => {
+    const bundleIndexes = PositionBundleUtil.getOccupiedBundleIndexes(positionBundle);
+    bundleIndexes.forEach((bundleIndex) => {
+      const pda = PDAUtil.getBundledPosition(ctx.program.programId, positionBundle.positionBundleMint, bundleIndex);
+      bundledPositionPubkeys.push(pda.publicKey);
+    });
+  });
+
+  // Fetch bundled positions
+  const bundledPositionData = await ctx.fetcher.getPositions(bundledPositionPubkeys, IGNORE_CACHE);
+
+  return positionBundles.map(([positionBundleAddress, positionBundleData]) => {
+    const bundleIndexes = PositionBundleUtil.getOccupiedBundleIndexes(positionBundleData);
+    const bundledPositions = new Map(
+      bundleIndexes
+      .map((bundleIndex) => {
+        const pda = PDAUtil.getBundledPosition(ctx.program.programId, positionBundleData.positionBundleMint, bundleIndex);
+        return [bundleIndex, bundledPositionData.get(AddressUtil.toString(pda.publicKey))];
+      })
+      .filter(([_, v]) => v !== null) as [number, PositionData][]
+    );
+
+    return {
+      positionBundleAddress,
+      positionBundleData,
+      bundledPositions,
+    }
+  }); 
 }

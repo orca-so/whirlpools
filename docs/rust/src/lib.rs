fa72f0aac7da2a09ddeabbe405dce86ddc78929e
Will <82029448+wjthieme@users.noreply.github.com>
2024-10-30 21:57:29 -0400

    Include low-level sdk in rustdoc/typedoc (#429)
    * Include low-level sdk in rustdoc/typedoc
    
    * Change navbar organization (#436)
    
    Co-authored-by: calintje <csimon@orca.so>
    
    ---------
    
    Co-authored-by: Calin <57701767+calintje@users.noreply.github.com>
    Co-authored-by: calintje <csimon@orca.so>

---
+++ docs/rust/src/lib.rs
@@ -0,0 +1,16 @@
+#![doc = include_str!("../README.md")]
+
+pub mod orca_whirlpools_core {
+    #![doc = include_str!("../../../rust-sdk/core/README.md")]
+    pub use orca_whirlpools_core::*;
+}
+
+pub mod orca_whirlpools_client {
+    #![doc = include_str!("../../../rust-sdk/client/README.md")]
+    pub use orca_whirlpools_client::*;
+}
+
+pub mod orca_whirlpools {
+    #![doc = include_str!("../../../rust-sdk/whirlpool/README.md")]
+    pub use orca_whirlpools::*;
+}

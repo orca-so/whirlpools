4ba7d480e576333fd9814edb30c561dafc645a28
Wilhelm Thieme <wjthieme@gmail.com>
2024-11-01 09:50:03 -0400

    Tests

--- ts-sdk/integration/index.test.ts
+++ ts-sdk/integration/index.test.ts
@@ -4,11 +4,13 @@ import { readdirSync } from "fs";
 import { describe, it } from "vitest";
 
 const commandTemplates = [
-  "tsx ./index.ts --tsconfig {}",
-  "ts-node ./index.ts --tsconfig {}",
-  "tsc --tsconfig {} --outDir ./dist && node ./dist/index.js"
+  "tsx --tsconfig {} ./index.ts",
+  "tsc --project {} --outDir ./dist && node ./dist/index.js",
+  // FIXME: ts-node does not play nice with ESM since node 20
+  // "ts-node --esm --project {} ./index.ts",
 ]
 
+// commonjs not included here because wasm wouldn't support it
 const tsConfigs = readdirSync("./configs");
 
 function execute(command: string): Promise<string> {
@@ -24,7 +26,7 @@ describe("Integration", () => {
   commandTemplates.forEach(template => {
     tsConfigs.forEach(config => {
       const command = template.replace("{}", `./configs/${config}`);
-      it(`Run using '${command}'`, async () => {
+      it(`Use '${command}'`, async () => {
         const stdout = await execute(command);
         assert(stdout.includes("Whirlpools"));
       });

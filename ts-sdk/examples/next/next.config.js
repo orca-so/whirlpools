21b6cb0aed2e64af5e0117ec5a14544b728451e4
Wilhelm Thieme <wjthieme@gmail.com>
2024-11-20 11:56:20 -0500

    Tweaks

--- ts-sdk/examples/next/next.config.js
+++ ts-sdk/examples/next/next.config.js
@@ -2,11 +2,13 @@ import NextBundleAnalyzer from "@next/bundle-analyzer";
 import CopyWebpackPlugin from "copy-webpack-plugin";
 
 const nextConfig = {
+  serverExternalPackages: ["@orca-so/whirlpools-core"],
   webpack(config, { isServer }) {
     config.experiments.asyncWebAssembly = true;
 
     // Copy `orca_whirlpools_core_js_bindings_bg.wasm` file
     // This is only needed because of the monorepo setup
+    // (local dependencies are symlinked and next doesn't like that)
     config.plugins.push(
       new CopyWebpackPlugin({
         patterns: [{
@@ -16,7 +18,7 @@ const nextConfig = {
       })
     );
 
-    // The following supresses a warning about using top-level-await
+    // The following supresses a warning about using top-level-await and is optional
     if (!isServer) {
       config.output.environment = { ...config.output.environment, asyncFunction: true };
     }

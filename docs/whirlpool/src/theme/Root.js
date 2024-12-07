39a1eb6c1d162b22063fba900b810cf78bdf21cf
calintje <csimon@orca.so>
2024-12-07 05:44:11 +0100

    Add animated button. Survey opens on button click

--- docs/whirlpool/src/theme/Root.js
+++ docs/whirlpool/src/theme/Root.js
@@ -1,11 +1,26 @@
-import React, { useEffect } from "react";
-import { useLocation } from "@docusaurus/router";
+import React, { useState, useEffect } from "react";
 
 export default function Root({ children }) {
-  const location = useLocation();
+  const [isHovered, setIsHovered] = useState(false);
+
+  const buttonStyle = {
+    padding: "10px 20px",
+    backgroundColor: "#FFD15C",
+    color: "#13174D",
+    fontSize: "18px",
+    fontWeight: "500",
+    border: "1px solid #13174D",
+    borderRadius: "8px",
+    cursor: "pointer",
+    boxShadow: "0 6px 12px rgba(0, 0, 0, 0.2)",
+    transition: "transform 0.2s ease, box-shadow 0.2s ease",
+    ...(isHovered && {
+      transform: "translate(2px, 1px)",
+      boxShadow: "None",
+    }),
+  };
 
   useEffect(() => {
-    // Add the Tally script globally to your site
     const script = document.createElement("script");
     script.src = "https://tally.so/widgets/embed.js";
     script.async = true;
@@ -19,28 +34,27 @@ export default function Root({ children }) {
   return (
     <>
       {children}
-      {(
-        <div style={{ position: "fixed", bottom: "20px", right: "20px", zIndex: 1000 }}>
-          <button
-            data-tally-open="mRZqGP"
-            data-tally-width="405"
-            data-tally-emoji-text="👋"
-            data-tally-emoji-animation="wave"
-            data-tally-auto-close="0"
-            style={{
-              padding: "10px 20px",
-              backgroundColor: "#007bff",
-              color: "white",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)"
-            }}
-          >
-            Share Feedback
-          </button>
-        </div>
-      )}
+      <div
+        style={{
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          zIndex: 1000,
+        }}
+      >
+        <button
+          data-tally-open="mRZqGP"
+          data-tally-width="405"
+          data-tally-emoji-text="👋"
+          data-tally-emoji-animation="wave"
+          data-tally-auto-close="0"
+          style={buttonStyle}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          Share Feedback
+        </button>
+      </div>
     </>
   );
 }

790df8158e3519938b25001ff7c66d879075e8ee
Wilhelm Thieme <wjthieme@gmail.com>
2025-01-06 11:40:02 -0500

    Tweak

--- rust-sdk/client/codama.js
+++ rust-sdk/client/codama.js
@@ -4,14 +4,14 @@ import { rootNodeFromAnchor } from "@codama/nodes-from-anchor";
 import { readFileSync } from "fs";
 
 const idl = JSON.parse(readFileSync("../../target/idl/whirlpool.json", "utf8"));
-const node = rootNodeFromAnchor(idl);
-const visitor = renderVisitor("./src/generated");
-// IDL generated with anchor 0.29 does not have the address field so we have to add it manually
-const codama = createFromRoot({
-  ...node,
-  program: {
-    ...node.program,
-    publicKey: "whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc",
-  },
+// IDL generated with anchor 0.29 does not have the metadata field so we have to add it manually
+const node = rootNodeFromAnchor({
+  ...idl,
+  metadata: {
+    address: "whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc",
+    origin: "anchor"
+  }
 });
+const visitor = renderVisitor("./src/generated");
+const codama = createFromRoot(node);
 codama.accept(visitor);

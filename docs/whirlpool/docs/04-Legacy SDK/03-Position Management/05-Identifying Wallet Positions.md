21090e4f879f80e7cb29c40de3c7448b0e37afb2
calintje <calsim113@gmail.com>
2025-04-11 21:05:57 +0200

    Finish restructuring. Add actions to examples. Add tx-sender to docs.

--- docs/whirlpool/docs/04-Legacy SDK/03-Position Management/05-Identifying Wallet Positions.md
+++
@@ -1,2 +0,0 @@
-# Identifying Wallet Positions
-To fetch all position accounts of a wallet, you can use [`getAllPositionAccountsByOwner`](https://dev.orca.so/legacy/functions/getAllPositionAccountsByOwner.html).
--- rust-sdk/core/src/math/tick_array.rs
+++ rust-sdk/core/src/math/tick_array.rs
@@ -6,7 +6,7 @@ use crate::{
 
 use super::{
     get_initializable_tick_index, get_next_initializable_tick_index,
-    get_prev_initializable_tick_index, get_tick_array_start_tick_index,
+    get_prev_initializable_tick_index,
 };
 
 #[derive(Clone, Debug, PartialEq, Eq)]

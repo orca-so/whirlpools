9f3a4e6c2cc26d62efae8448d97db215339f45ba
Will <82029448+wjthieme@users.noreply.github.com>
2025-05-05 09:42:07 -0500

    Add swap feature flag to core sdk (#931)

--- rust-sdk/core/src/types/mod.rs
+++ rust-sdk/core/src/types/mod.rs
@@ -5,12 +5,14 @@ mod position;
 mod rewards;
 mod swap;
 mod tick;
-mod tick_array;
 mod token;
 mod u128;
 
 #[cfg(feature = "wasm")]
 mod u64;
+
+#[cfg(feature = "swap")]
+mod tick_array;
 
 pub use fees::*;
 pub use liquidity::*;
@@ -19,9 +21,11 @@ pub use position::*;
 pub use rewards::*;
 pub use swap::*;
 pub use tick::*;
-pub use tick_array::*;
 pub use token::*;
 pub use u128::*;
 
 #[cfg(feature = "wasm")]
 pub use u64::*;
+
+#[cfg(feature = "swap")]
+pub use tick_array::*;

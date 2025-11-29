6dc2e47fe53cebd78bcec0cbe32f32addb0dea8f
shio 🚀 <jlshiohira23@gmail.com>
2025-11-28 22:28:18 -0600

    add set_adaptive_fee_constants ix

--- legacy-sdk/whirlpool/src/instructions/adaptive-fee/index.ts
+++ legacy-sdk/whirlpool/src/instructions/adaptive-fee/index.ts
@@ -5,3 +5,4 @@ export * from "./set-delegated-fee-authority-ix";
 export * from "./set-fee-rate-by-delegated-fee-authority-ix";
 export * from "./set-initialize-pool-authority-ix";
 export * from "./set-preset-adaptive-fee-constants-ix";
+export * from "./set-adaptive-fee-constants-ix";
